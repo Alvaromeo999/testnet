b7a4cabf7d593120bf4045ccb8c38c81db1460e5
Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>
2026-02-24 23:57:59 +0700

    Refactor index.tsx to include Router and CSS imports
    Signed-off-by: Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,9 @@
-import App from './App'; // Ubah dari Shop ke App
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './defaults.css';
+import App from './App'; // Berubah dari Shop ke App
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
